a089df7b29d75d8f56654b0e8c19487a416810db
Michael Dillon <mic.dillon@gmail.com>
2012-06-17 11:02:39 +0200

    problem 12 now runs in under a minute

--- euler.hs
+++ euler.hs
@@ -3,14 +3,30 @@
 -- -- a collection of functions that seem to recure in solutions
 -- -- to euler project problems.
 --
-
 module Euler 
 (
   dgts
+, dvsrs
 ) where
+
+import Control.Applicative
+import Data.List
 
 dgts :: Integer -> [Integer]
 dgts n
   | a == 0    = [(n `mod` 10)]
   | otherwise = dgts a ++ [(n `mod` 10)]
   where a = n `div` 10
+
+-- somewhat efficient function for getting the
+-- divisors of a number (need to get rid of the 
+-- call to nub at some point).
+dvsrs :: (Eq a, Num a, Integral a) => a -> [a]
+dvsrs n = nub $ dv n 2
+  where
+    dv n i
+      | n == 1    = [1]
+      | i == n    = [1,n]
+      | d         = (*) <$> dv (n`div`i) 2 <*> [1,i]
+      | otherwise = dv n (i+1)
+        where d = n`mod`i==0
--- problem12.hs
+++ problem12.hs
@@ -1,16 +1,10 @@
 --
 -- problem12.hs
 --
-import Control.Applicative
+module Main where
 
-dvsrs :: (Eq a, Num a, Integral a) => a -> [a]
-dvsrs n = hlpr n n []
-  where 
-    hlpr n i ds
-      | i == 1    = 1 : ds
-      | a == True = hlpr n (i-1) (i:ds)
-      | otherwise = hlpr n (i-1) ds
-        where a = n `mod` i == 0
+import Euler
+import Control.Applicative
 
 -- exploiting the fact that the nth triangle number
 -- equals (n*(n+1))/2. 
@@ -19,8 +13,6 @@ tdvs n
   | even n    = (*) <$> dvsrs (n`div`2) <*> dvsrs (n+1)
   | otherwise = (*) <$> dvsrs n <*> dvsrs ((n+1)`div`2)
 
--- still need some optimization because ans
--- takes about 4 minutes to run
 ans = hlpr 1
   where
     hlpr n
@@ -28,3 +20,6 @@ ans = hlpr 1
       | otherwise      = hlpr (n+1)
         where 
           t = tdvs n
+
+main = do
+  putStrLn ("answer: " ++ show ans)
